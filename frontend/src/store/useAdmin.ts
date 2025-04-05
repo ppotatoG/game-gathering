4ebb055eb8cf16f0fee0094577bc1f747a55212c
ppotatoG <thgus2270@gmail.com>
2025-04-05 21:01:45 +0900

    feat(frontend): add callback support to store for login user and model info

--- frontend/src/store/useAdmin.ts
+++ frontend/src/store/useAdmin.ts
@@ -1,9 +1,22 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-export const useAdminStore = create<AdminState>(set => ({
-    isAdmin: false,
-    auctionCode: '',
-    auctionInfo: null,
-    setAdmin: auction => set({ isAdmin: true, auctionCode: auction.code, auctionInfo: auction }),
-    logoutAdmin: () => set({ isAdmin: false, auctionCode: '', auctionInfo: null })
-}));
+export const useAdminStore = create<AdminState>()(
+    persist(
+        set => ({
+            isAdmin: false,
+            auctionCode: '',
+            auctionInfo: null,
+            setAdmin: auction =>
+                set({
+                    isAdmin: true,
+                    auctionCode: auction.code,
+                    auctionInfo: auction
+                }),
+            logoutAdmin: () => set({ isAdmin: false, auctionCode: '', auctionInfo: null })
+        }),
+        {
+            name: 'admin-store'
+        }
+    )
+);
