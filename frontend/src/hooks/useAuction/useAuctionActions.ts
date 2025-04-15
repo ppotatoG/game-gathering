b4337304a2b2ecd18ea73d3fbf08bab31d6521d0
ppotatoG <thgus2270@gmail.com>
2025-04-15 22:40:08 +0900

    feat(frontend): update useAuctionActions with init and bid emitters
    - emitInit: 경매 초기화 이벤트 연결
    - emitBid: auction:input, chatMessage 이중 emit 처리 (채팅이 UI에 안 보이던 이슈 해결)

--- frontend/src/hooks/useAuction/useAuctionActions.ts
+++ frontend/src/hooks/useAuction/useAuctionActions.ts
@@ -1,6 +1,11 @@
 import socket from '@/lib/socket';
 
 export const useAuctionActions = ({ code, nickname }: { code: string; nickname: string }) => {
+    const emitInit = () => {
+        console.log('[소켓] 경매 초기화');
+        socket.emit('auction:reset', { auctionCode: code });
+    };
+
     const emitStart = (round: number, targetUser: string) => {
         socket.emit('auction:start', {
             auctionId: code,
@@ -9,16 +14,25 @@ export const useAuctionActions = ({ code, nickname }: { code: string; nickname: 
         });
     };
 
-    const emitBid = (point: number, teamId: string) => {
+    const emitBid = (bid: Omit<Bid, 'nickname'>) => {
         socket.emit('auction:input', {
-            auctionId: code,
-            bid: { user: nickname, point, teamId }
+            auctionCode: code,
+            bid: {
+                nickname,
+                ...bid
+            }
+        });
+
+        socket.emit('chatMessage', {
+            user: nickname,
+            message: String(bid.point)
         });
     };
 
     const emitNextUser = () => {
+        console.log('[소켓] 다음 유저');
         socket.emit('auction:next-user', { auctionCode: code });
     };
 
-    return { emitStart, emitBid, emitNextUser };
+    return { emitInit, emitStart, emitBid, emitNextUser };
 };
