33046ca013392dcf6a19e9bac1417dfd9948bfb5
ppotatoG <thgus2270@gmail.com>
2025-11-04 12:33:34 +0900

    불필요한 import 삭제

--- backend/src/sockets/handlers/connectionHandler.ts
+++ backend/src/sockets/handlers/connectionHandler.ts
@@ -1,14 +1,9 @@
-import { Server as IOServer, Server } from 'socket.io';
+import { Server as Server } from 'socket.io';
 
 import { nicknameMap } from '../stores/nicknameMap';
 
 import AuctionUser, { AuctionUserDocument } from '@/models/AuctionUser';
-import {
-    AuctionSocket,
-    ClientToServerEvents,
-    ServerToClientEvents,
-    AuctionSocketData,
-} from '@/types/socket';
+import { AuctionSocket } from '@/types/socket';
 
 export default function registerConnectionHandlers(io: Server, socket: AuctionSocket) {
     socket.on('auction:check-nickname', async ({ auctionCode, nickname }, callback) => {
